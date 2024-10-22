b9cccbb35adcb4921be19bce80b009dbdb2df4e1
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2024-10-22 12:55:14 +0200

    Create GenreDTO

---
+++ src/main/java/app/dtos/GenreDTO.java
@@ -0,0 +1,15 @@
+package app.dtos;
+
+import lombok.*;
+
+@Getter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class GenreDTO {
+
+    private Long id;
+    private String name;
+
+}
