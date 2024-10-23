54957aa4305050cf2668d8e9f6eb797a2241dd71
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-10-23 15:59:18 +0200

    Add security route tests

---
+++ src/test/java/app/route/SecurityRoutesTest.java
@@ -0,0 +1,119 @@
+package app.route;
+
+import app.Populator;
+import app.SecurityTestUtil;
+import app.config.AppConfig;
+import app.config.HibernateConfig;
+import app.dto.UserDTO;
+import app.entity.Role;
+import app.entity.User;
+import io.javalin.Javalin;
+import io.restassured.RestAssured;
+import jakarta.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.*;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+
+class SecurityRoutesTest {
+
+    private static Populator populator;
+    private static Javalin app;
+
+    @BeforeAll
+    static void beforeAll() {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryForTest();
+        int port = 7070;
+
+        populator = new Populator(emf);
+        app = AppConfig.startServer(port, emf);
+
+        RestAssured.baseURI = String.format("http://localhost:%d/api", port);
+    }
+
+    @BeforeEach
+    void setUp() {
+        List<Role> roles = populator.createRoles();
+        populator.persist(roles);
+
+        List<User> users = populator.createUsers(roles);
+        populator.persist(users);
+    }
+
+    @AfterEach
+    void tearDown() {
+        populator.cleanup(User.class);
+        populator.cleanup(Role.class);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        AppConfig.stopServer(app);
+    }
+
+    @Test
+    void test() {
+        given()
+                .when()
+                .get("/auth/test")
+                .then()
+                .statusCode(200)
+                .body("message", equalTo("Hello from open."));
+    }
+
+    @Test
+    void login() {
+        UserDTO userDTO = new UserDTO("User1", "1234");
+
+        given()
+                .body(userDTO)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .body("username", equalTo(userDTO.getUsername()))
+                .body("token", is(notNullValue()));
+    }
+
+    @Test
+    void register() {
+        UserDTO userDTO = new UserDTO("User3", "1234");
+
+        given()
+                .body(userDTO)
+                .when()
+                .post("/auth/register")
+                .then()
+                .statusCode(201)
+                .body("username", equalTo(userDTO.getUsername()))
+                .body("token", is(notNullValue()));
+    }
+
+    @Test
+    void protectedUser() {
+        String token = SecurityTestUtil.loginAccount("User1", "1234");
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get("/protected/user_demo")
+                .then()
+                .statusCode(200)
+                .body("message", equalTo("Hello from user protected."));
+    }
+
+    @Test
+    void protectedAdmin() {
+        String token = SecurityTestUtil.loginAccount("Admin1", "1234");
+
+        given()
+                .header("Authorization", "Bearer " + token)
+                .when()
+                .get("/protected/admin_demo")
+                .then()
+                .statusCode(200)
+                .body("message", equalTo("Hello from admin protected."));
+    }
+}
