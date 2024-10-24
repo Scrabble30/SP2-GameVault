091990763426adb6813267df3763d3512fa8c5dc
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2024-10-24 11:54:13 +0200

    adds review routes

--- src/main/java/app/route/Routes.java
+++ src/main/java/app/route/Routes.java
@@ -1,11 +1,21 @@
 package app.route;
 
 import io.javalin.apibuilder.EndpointGroup;
+import jakarta.persistence.EntityManagerFactory;
+
+import static io.javalin.apibuilder.ApiBuilder.path;
 
 public class Routes {
 
+    private final ReviewRoute reviewRoute;
+
+    public Routes(EntityManagerFactory emf) {
+        reviewRoute = new ReviewRoute(emf);
+    }
+
     public EndpointGroup getAPIRoutes() {
         return () -> {
+            path("/reviews", reviewRoute.getRoutes());
         };
     }
 }
