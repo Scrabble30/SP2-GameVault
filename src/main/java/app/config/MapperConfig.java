0b9b69e76fb2ca7333357320f014d380612a0108
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2024-10-25 01:20:32 +0200

    Defined the matching strategy for the mapper class

--- src/main/java/app/config/MapperConfig.java
+++ src/main/java/app/config/MapperConfig.java
@@ -1,13 +1,33 @@
 package app.config;
 
 
+import app.dto.ReviewDTO;
+import app.entity.Review;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.modelmapper.convention.MatchingStrategies;
 
 public class MapperConfig {
     private static final ModelMapper modelMapper = new ModelMapper();
 
     public static ModelMapper getModelMapper() {
-        return (ModelMapper) modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        //Set matching strategy
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        //Or define custom mappings here if needed
+        /*
+        modelMapper.addMappings(new PropertyMap<Review, ReviewDTO>() {
+            @Override
+            protected void configure() {
+                map().setId(source.getId());
+                map().setUsername(source.getUsername());
+                map().setGameId(source.getGameId());
+                map().setRating(source.getRating());
+                map().setReview(source.getReview());
+            }
+        });
+         */
+
+        return modelMapper;
     }
 }
