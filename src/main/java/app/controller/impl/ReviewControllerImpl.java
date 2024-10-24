69708faef7038d66790a352728b06b9cd3253f35
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2024-10-24 13:02:18 +0200

    in progress: implementing read method

--- src/main/java/app/controller/impl/ReviewControllerImpl.java
+++ src/main/java/app/controller/impl/ReviewControllerImpl.java
@@ -2,11 +2,15 @@ package app.controller.impl;
 
 import app.controller.Controller;
 import app.dao.impl.ReviewDAOImpl;
+import app.dto.ReviewDTO;
+import app.entity.Review;
+import app.mapper.ReviewMapper;
 import io.javalin.http.Context;
 import jakarta.persistence.EntityManagerFactory;
 
 public class ReviewControllerImpl implements Controller {
     private final ReviewDAOImpl reviewDAO;
+    private final ReviewMapper reviewMapper = new ReviewMapper();
 
     public ReviewControllerImpl(EntityManagerFactory emf) {
         this.reviewDAO = ReviewDAOImpl.getInstance(emf);
@@ -14,7 +18,13 @@ public class ReviewControllerImpl implements Controller {
 
     @Override
     public void read(Context ctx) {
+        long id = ctx.pathParamAsClass("id", Long.class).get();
 
+        Review review = reviewDAO.getById(id);
+        ReviewDTO reviewDTO = reviewMapper.convertToDTO(review);
+
+        ctx.res().setStatus(200);
+        ctx.json(reviewDTO, ReviewDTO.class);
     }
 
     @Override
