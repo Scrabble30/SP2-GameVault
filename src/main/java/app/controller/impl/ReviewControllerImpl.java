1a4108936de76c3070ec57e86f5a60383c497c79
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2024-10-25 01:21:20 +0200

    In progress: implemented methods, refactored names and added unused logger

--- src/main/java/app/controller/impl/ReviewControllerImpl.java
+++ src/main/java/app/controller/impl/ReviewControllerImpl.java
@@ -7,8 +7,15 @@ import app.entity.Review;
 import app.mapper.ReviewMapper;
 import io.javalin.http.Context;
 import jakarta.persistence.EntityManagerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ReviewControllerImpl implements Controller {
+    private final Logger logger = LoggerFactory.getLogger(ReviewControllerImpl.class);
+
     private final ReviewDAOImpl reviewDAO;
     private final ReviewMapper reviewMapper = new ReviewMapper();
 
@@ -20,30 +27,55 @@ public class ReviewControllerImpl implements Controller {
     public void read(Context ctx) {
         long id = ctx.pathParamAsClass("id", Long.class).get();
 
-        Review review = reviewDAO.getById(id);
-        ReviewDTO reviewDTO = reviewMapper.convertToDTO(review);
+        Review foundReview = reviewDAO.getById(id);
+        ReviewDTO mappedReviewDTO = reviewMapper.convertToDTO(foundReview);
 
         ctx.res().setStatus(200);
-        ctx.json(reviewDTO, ReviewDTO.class);
+        ctx.json(mappedReviewDTO, ReviewDTO.class);
     }
 
     @Override
     public void readAll(Context ctx) {
+        long id = ctx.pathParamAsClass("id", Long.class).get();
 
+        Set<Review> foundReviewSet = reviewDAO.getByGameId(id);
+        Set<ReviewDTO> mappedReviewDTOSet = foundReviewSet.stream().map(reviewMapper::convertToDTO).collect(Collectors.toSet());
+
+        ctx.res().setStatus(200);
+        ctx.json(mappedReviewDTOSet, ReviewDTO.class);
     }
 
     @Override
     public void create(Context ctx) {
+        ReviewDTO reviewDTO = ctx.bodyAsClass(ReviewDTO.class);
+        Review mappedReview = reviewMapper.convertToEntity(reviewDTO);
 
+        Review createdReview = reviewDAO.create(mappedReview);
+        ReviewDTO mappedReviewDTO = reviewMapper.convertToDTO(createdReview);
+
+        ctx.res().setStatus(201);
+        ctx.json(mappedReviewDTO, ReviewDTO.class);
     }
 
     @Override
     public void update(Context ctx) {
+        long id = ctx.pathParamAsClass("id", Long.class).get();
+        ReviewDTO reviewDTO = ctx.bodyAsClass(ReviewDTO.class);
+        Review mappedReview = reviewMapper.convertToEntity(reviewDTO);
 
+        Review updatedReview = reviewDAO.update(id, mappedReview);
+        ReviewDTO mappedReviewDTO = reviewMapper.convertToDTO(updatedReview);
+
+        ctx.res().setStatus(200);
+        ctx.json(mappedReviewDTO, ReviewDTO.class);
     }
 
     @Override
     public void delete(Context ctx) {
+        long id = ctx.pathParamAsClass("id", Long.class).get();
 
+        reviewDAO.delete(id);
+
+        ctx.res().setStatus(204);
     }
 }
