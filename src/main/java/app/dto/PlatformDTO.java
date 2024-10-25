5acf1c0d62a8cedfad25f1fe2022052e2fa9253a
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-10-25 10:58:44 +0200

    Optimize imports

--- src/main/java/app/dto/PlatformDTO.java
+++ src/main/java/app/dto/PlatformDTO.java
@@ -1,7 +1,9 @@
 package app.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
