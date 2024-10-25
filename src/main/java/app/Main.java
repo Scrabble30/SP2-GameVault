372115b24ca3d0ea42f9f44924a608046f20980d
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2024-10-25 23:59:13 +0200

    Implemented populator class to populate basic data to database

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -8,6 +8,10 @@ public class Main {
 
     public static void main(String[] args) {
         EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory("game_vault");
+
+        Populator populator = new Populator(emf);
+        populator.populateData();
+
         AppConfig.startServer(7070, emf);
     }
 }
