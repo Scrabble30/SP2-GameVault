25878af71cf6ce80cc7d1245a5a5944e354d349f
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2024-10-25 23:08:44 +0200

    Implemented all methods in GenreControllerImpl

--- src/main/java/app/controller/impl/GenreControllerImpl.java
+++ src/main/java/app/controller/impl/GenreControllerImpl.java
@@ -2,9 +2,19 @@ package app.controller.impl;
 
 import app.controller.Controller;
 import app.dao.impl.GenreDAOImpl;
+import app.dto.GenreDTO;
+import app.dto.PlatformDTO;
+import app.entity.Genre;
 import app.mapper.GenreMapper;
-import io.javalin.http.Context;
+import io.javalin.http.*;
+import io.javalin.validation.ValidationException;
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityNotFoundException;
+import org.hibernate.MappingException;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class GenreControllerImpl implements Controller {
 
@@ -28,26 +38,89 @@ public class GenreControllerImpl implements Controller {
 
     @Override
     public void create(Context ctx) {
+        try {
+            GenreDTO genreDTO = ctx.bodyValidator(GenreDTO.class).get();
+            Genre mappedGenre = genreMapper.convertToEntity(genreDTO);
 
+            Genre createdGenre = genreDAO.create(mappedGenre);
+            GenreDTO mappedGenreDTO = genreMapper.convertToDTO(createdGenre);
+
+            ctx.status(HttpStatus.CREATED);
+            ctx.json(mappedGenreDTO, GenreDTO.class);
+        } catch (ValidationException e) {
+            throw new BadRequestResponse(e.getErrors().toString());
+        } catch (MappingException e) {
+            throw new BadRequestResponse(e.getMessage());
+        } catch (EntityExistsException e) {
+            throw new ConflictResponse(e.getMessage());
+        }
     }
 
     @Override
     public void getById(Context ctx) {
+        try {
+            Long id = ctx.pathParamAsClass("id", Long.class).get();
 
+            Genre genre = genreDAO.getById(id);
+            GenreDTO mappedGenreDTO = genreMapper.convertToDTO(genre);
+
+            ctx.status(HttpStatus.OK);
+            ctx.json(mappedGenreDTO, PlatformDTO.class);
+        } catch (ValidationException e) {
+            throw new BadRequestResponse(e.getErrors().toString());
+        } catch (MappingException e) {
+            throw new BadRequestResponse(e.getMessage());
+        } catch (EntityNotFoundException e) {
+            throw new NotFoundResponse(e.getMessage());
+        }
     }
 
     @Override
     public void getAll(Context ctx) {
+        try {
+            Set<Genre> genreSet = genreDAO.getAll();
+            Set<GenreDTO> mappedGenreDTOSet = genreSet.stream().map(genreMapper::convertToDTO).collect(Collectors.toSet());
 
+            ctx.status(HttpStatus.OK);
+            ctx.json(mappedGenreDTOSet, GenreDTO.class);
+        } catch (MappingException e) {
+            throw new BadRequestResponse(e.getMessage());
+        }
     }
 
     @Override
     public void update(Context ctx) {
+        try {
+            Long id = ctx.pathParamAsClass("id", Long.class).get();
+            GenreDTO genreDTO = ctx.bodyValidator(GenreDTO.class).get();
+            Genre mappedGenre = genreMapper.convertToEntity(genreDTO);
 
+            Genre updatedGenre = genreDAO.update(id, mappedGenre);
+            GenreDTO mappedGenreDTO = genreMapper.convertToDTO(updatedGenre);
+
+            ctx.status(HttpStatus.OK);
+            ctx.json(mappedGenreDTO, GenreDTO.class);
+        } catch (ValidationException e) {
+            throw new BadRequestResponse(e.getErrors().toString());
+        } catch (MappingException e) {
+            throw new BadRequestResponse(e.getMessage());
+        } catch (EntityNotFoundException e) {
+            throw new NotFoundResponse(e.getMessage());
+        }
     }
 
     @Override
     public void delete(Context ctx) {
+        try {
+            Long id = ctx.pathParamAsClass("id", Long.class).get();
 
+            genreDAO.delete(id);
+
+            ctx.status(HttpStatus.NO_CONTENT);
+        } catch (ValidationException e) {
+            throw new BadRequestResponse(e.getErrors().toString());
+        } catch (EntityNotFoundException e) {
+            throw new NotFoundResponse(e.getMessage());
+        }
     }
 }
