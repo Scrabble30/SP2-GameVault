eeaacb25594dad031fb7211319609ad8b4f9a633
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-10-25 19:56:23 +0200

    Create PlatformControllerImpl test

---
+++ src/main/java/app/controller/impl/PlatformControllerImpl.java
@@ -0,0 +1,32 @@
+package app.controller.impl;
+
+import app.controller.Controller;
+import io.javalin.http.Context;
+
+public class PlatformControllerImpl implements Controller {
+
+    @Override
+    public void create(Context ctx) {
+
+    }
+
+    @Override
+    public void getById(Context ctx) {
+
+    }
+
+    @Override
+    public void getAll(Context ctx) {
+
+    }
+
+    @Override
+    public void update(Context ctx) {
+
+    }
+
+    @Override
+    public void delete(Context ctx) {
+
+    }
+}
