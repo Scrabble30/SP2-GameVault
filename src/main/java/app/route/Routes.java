1f5b58e82113255484238fdf62be240da874364c
Scrabble30 <144219106+Scrabble30@users.noreply.github.com>
2024-10-25 20:41:42 +0200

    Add platform routes

--- src/main/java/app/route/Routes.java
+++ src/main/java/app/route/Routes.java
@@ -7,14 +7,17 @@ import static io.javalin.apibuilder.ApiBuilder.path;
 
 public class Routes {
 
+    private final PlatformRoutes platformRoutes;
     private final GameRoutes gameRoutes;
 
     public Routes(EntityManagerFactory emf) {
+        this.platformRoutes = new PlatformRoutes(emf);
         this.gameRoutes = new GameRoutes(emf);
     }
 
     public EndpointGroup getAPIRoutes() {
         return () -> {
+            path("/platforms", platformRoutes.getPlatformRoutes());
             path("/games", gameRoutes.getGameRoutes());
         };
     }
